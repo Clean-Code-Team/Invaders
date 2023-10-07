1cecc089317c7ae3ab644f4adeb66ad35c659725
Meongjeong <258xsw@naver.com>
2023-10-07 21:39:32 +0900

    feat: Add Ship design(shipB)

--- src/entity/Ship.java
+++ src/entity/Ship.java
@@ -36,9 +36,10 @@ public class Ship extends Entity {
 	 *            Initial position of the ship in the Y axis.
 	 */
 	public Ship(final int positionX, final int positionY) {
-		super(positionX, positionY, 13 * 2, 8 * 2, Color.GREEN);
+		super
+				(positionX, positionY, 13 * 2, 8 * 2, Color.GREEN);
 
-		this.spriteType = SpriteType.Ship;
+		this.spriteType = SpriteType.ShipB;
 		this.shootingCooldown = Core.getCooldown(SHOOTING_INTERVAL);
 		this.destructionCooldown = Core.getCooldown(1000);
 	}
